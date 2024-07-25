ea4997f2f693924e4d51a4d78e7e4dfe8e28c1c3
SKY_SHIMIZU <shimizu@skysystem.co.jp>
2024-07-25 17:37:33 +0900

    learn jsx syntax

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div><h1>Hello, World!</h1></div>;
+import React, { Component} from 'react';
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
